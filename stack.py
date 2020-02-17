aee3ad32072ac83976cb0eb6c72298f36bcd2ebe
Abel <abeljohnjoseph@gmail.com>
2020-02-16 19:53:52 -0500

    Create stack with list

---
+++ stack.py
@@ -0,0 +1,18 @@
+class stack_l:
+	"""Stack implemented using a list."""
+	def __init__(self):
+		self.items = []
+
+	def __repr__(self):
+		return f"stack({str(self.items)})"
+
+	def push(self,value):
+		self.items.append(value)
+
+	def pop(self):
+		popped_item = self.items[-1]
+		self.items = self.items[:-1]
+		return popped_item
+
+	def peek(self):
+		return self.items[-1]
