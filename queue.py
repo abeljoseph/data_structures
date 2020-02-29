2db1d2bbfe60cccceff11dda51088a9413e76f39
Abel Joseph <38389364+abeljoseph@users.noreply.github.com>
2020-02-29 11:02:23 -0500

    Fix formatting

--- queue.py
+++ queue.py
@@ -1,10 +1,10 @@
 class queue:
-	  """Queue implemented using a list."""
-	  def __init__(self):
+    """Queue implemented using a list."""
+    def __init__(self):
         self.items = []
 
 class Node:
-	  """General node for linked list implementation."""
-	  def __init__(self, data=None):
-	      self.data = data
-		    self.next = None
+    """General node for linked list implementation."""
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
